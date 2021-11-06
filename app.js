71bab9f8c4d9cfda357366aee33e3a369f80ea16
diogoschen <55403415+diogoschen@users.noreply.github.com>
2021-11-06 13:52:06 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const innerBox = document.querySelectorAll('.innerBox');
+const boxFront = document.querySelectorAll('.boxFront');
+const bottom = document.querySelector('.innerBottom');
+const boxBack = document.querySelectorAll('.boxBackInner');
+const button = document.querySelector('button');
+const form = document.querySelectorAll('form');
+innerBox.forEach((val, i) => {
+    boxFront[i].addEventListener('click', (e) => {
+        innerBox[i].style.transform = 'rotateY(180deg)';
+        bottom.style.transform = 'rotateY(180deg)';
+    });
+
+    boxBack[i].addEventListener('click', (e) => {
+        innerBox[i].style.transform = 'rotateY(0deg)';
+        bottom.style.transform = 'rotateY(0deg)';
+    });
+});
+
+button.addEventListener('click', (e) => {
+    form[2][0].value = form[0].elements[0].value;
+    form[2][1].value = form[1].elements[0].value;
+    form[2].submit();
+});
+
+if (window.location.href.split('s')[1][0] === 'u') {
+    window.location('/')
+};
