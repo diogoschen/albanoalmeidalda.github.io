2695c495bd7ee6d2c71896177065ebc66c7cf218
natalsilvabarbosachen <55403415+natalsilvabarbosachen@users.noreply.github.com>
2021-12-23 04:26:01 +0000

    Add files via upload

--- natal2021/app.js
+++ natal2021/app.js
@@ -13,23 +13,29 @@ const containerFinal = document.querySelector('.container-3')
 let audioClick = 0;
 let nextClick = 0;
 
+
+
 const containers = document.querySelectorAll('.container')
-// containers.forEach(container => {
-//     container.style.opacity = 0;
+containers.forEach(container => {
+    container.style.opacity = 0;
 
-// })
-
-// containers[0].style.opacity = 1;
+})
+containers[0].style.opacity = 1;
+const loadingTime = 000
+timeDisappear(containers[0].children[0], 'on', loadingTime)
 
 next.addEventListener('click', () => {
     nextClick++
     // containers[nextClick - 1].style.opacity = 0;
-    containers[nextClick - 1].classList.add('container-animation');
     containers[nextClick].style.opacity = 1;
-
-    if (nextClick === 2) {
+    document.querySelector('#tsparticles').classList.add('confetti-animation')
+    containers[nextClick - 1].children[0].style.transform = 'translateZ(1px)'
+    containers[nextClick - 1].classList.add('container-animation');
+    if (nextClick === 3) {
         next.style.opacity = 0;
         timeDisappear(next)
+    } else {
+        containers[nextClick].children[0].classList.remove('display-none')
     }
 })
 
@@ -40,10 +46,20 @@ for (let i = 0; i < 170; i++) {
     containerFinal.append(div)
 }
 
-function timeDisappear(element) {
-    setTimeout(() => {
-        element.classList.add('display-none')
-    }, 1000)
+function timeDisappear(element, state = 'off', timeOut = 100) {
+    if (state === 'on') {
+        element.style.opacity = 1;
+        setTimeout(() => {
+            element.classList.remove('display-none')
+        }, timeOut)
+    }
+    else {
+        element.style.opacity = 1;
+        setTimeout(() => {
+            containers[0].style.opacity = 0;
+            element.classList.add('display-none')
+        }, timeOut)
+    }
 }
 
 audioBtn.addEventListener('click', () => {
@@ -82,17 +98,6 @@ audioBtn.addEventListener('click', () => {
     }
 
 })
-
-
-const frameAround = `
-<div class="frame">
-    <section class="frame-top"></section>
-    <section class="frame-bottom"></section>
-    <section class="frame-left"></section>
-    <section class="frame-right"></section>
-</div>`
-
-
 
 
 containers.forEach(container => {
